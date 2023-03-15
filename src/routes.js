3a99777199467033ee02f8eaf961c90b7a0945ae
unknown <joseg081@gmail.com>
2023-03-15 17:56:08 -0300

    routes get, put and delete

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const UserController = require("./controllers/UserController");
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  }, 
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: UserController.getUserById,
+  }, 
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserController.updateUser,
+  }, 
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserController.deleteUser,
+  }, 
+]
